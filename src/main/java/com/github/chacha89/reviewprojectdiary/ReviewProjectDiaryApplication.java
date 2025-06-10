8aa779637e8a38f2efe4dd32c301bd9ec7f8e94d
cha cha cha <sungsuk855@gmail.com>
2025-06-10 14:40:32 +0900

    initial commit

---
+++ src/main/java/com/github/chacha89/reviewprojectdiary/ReviewProjectDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.github.chacha89.reviewprojectdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewProjectDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewProjectDiaryApplication.class, args);
+    }
+
+}
